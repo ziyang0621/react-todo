c7cb43cdd24476e841239e7ead1226ff930959fc
ziyang <ziyang0621@gmail.com>
2016-08-13 16:15:05 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+app.use(express.static('public'));
+
+app.listen(port, function () {
+  console.log('Express sever is up on port ' + port);
+});
